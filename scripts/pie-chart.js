e879ff46e066a6faae8e70b130ed99e96045a7f3
jewelb54 <44928545+jewelb54@users.noreply.github.com>
2023-12-06 14:18:12 -0500

    Piechart Commit

--- scripts/pie-chart.js
+++ scripts/pie-chart.js
@@ -7,7 +7,10 @@ class PieChart {
         this.height = 400;
         this.radius = Math.min(this.width, this.height) / 2;
     
-        this.color = d3.scaleOrdinal(d3.schemeCategory10);
+        this.color = d3.scaleOrdinal().range(['#ff5733', '#ffa833', '#33ff57', '#335eff', '#a833ff']);
+
+        // Define a variable to store the currently hovered slice
+        this.hoveredSlice = null;
     
         this.svg = d3.select("#pie-chart")
                 .attr("width", this.width)
@@ -19,22 +22,43 @@ class PieChart {
     
         this.arc = d3.arc()
                     .innerRadius(0)
-                    .outerRadius(this.radius);
+                    .outerRadius(this.radius)
+                    .cornerRadius(8); // Add corner radius for a better look;
     }
 
     draw() {
+
+        const chart = this;
+
         const arcs = this.svg.selectAll("arc")
                 .data(this.pie(this.data))
                 .enter()
                 .append("g")
-                .attr("class", "arc");
+                .attr("class", "arc")
+                .on("mouseover", function (event, d) {
+                        // Highlight the current slice on mouseover
+                        d3.select(this)
+                        .transition()
+                        .duration(200) // Adjust the duration as needed
+                        .attr("stroke", "black")
+                        .attr("stroke-width", 2);
+        
+                })
+                .on("mouseout", function (event, d) {
+                    // Remove the highlight on mouseout
+                    d3.select(this)
+                      .transition()
+                      .duration(200) // Adjust the duration as needed
+                      .attr("stroke", "none");
+
+                });
 
         arcs.append("path")
-                .attr("fill", (d, i) => this.color(i))
-                .attr("d", this.arc);
+                .attr("fill", (d, i) => chart.color(i))
+                .attr("d", chart.arc);
 
         arcs.append("text")
-                .attr("transform", d => `translate(${this.arc.centroid(d)})`)
+                .attr("transform", d => `translate(${chart.arc.centroid(d)})`)
                 .attr("text-anchor", d => (d.endAngle + d.startAngle) / 2 > Math.PI ? "end" : "start")
                 .text(d => `${d.data.gender}: ${Math.round(d.data.fatalityCount)}`)
                 .attr("fill", "white");
