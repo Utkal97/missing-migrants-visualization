eef1e22140e4298c4acce4401dc9b41e1537065f
Utkal97 <utkal.s15@iiits.in>
2023-12-11 21:24:46 -0500

    Symbol highlight in proportional symbol map

--- scripts/proportional-symbol-map.js
+++ scripts/proportional-symbol-map.js
@@ -20,7 +20,7 @@ function renderProportionalSymbolMap(csvData, stackedBarChart, pieChart) {
     const projection = d3.geoMercator();
     // Create path generator
     const path = d3.geoPath(projection);
-    let filterApplied = false;
+    let filterApplied = false, selectedRegion = null;
     d3.json('https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json').then(data => {
 
         svg.selectAll('path')
@@ -37,58 +37,82 @@ function renderProportionalSymbolMap(csvData, stackedBarChart, pieChart) {
             .enter().append('circle')
             .attr('cx', region => projection([region.coordinates[1], region.coordinates[0]])[0])
             .attr('cy', region => projection([region.coordinates[1], region.coordinates[0]])[1])
-            .attr('r', region => {
-                const radius = region.fatalityCount*0.001;
-                if(radius > 5)
-                    return radius;
-                else
-                    return region.fatalityCount*0.005;
-            })
+            .attr('r', region => computeRadius(region))
             .attr('fill', 'red')
-            .on('click', region => {
+            .on('click', function (region) {
+
                 d3.select('#region-text-container').selectAll('.region-text').remove();
 
-                // svg.selectAll('.region-text').remove();
-                
+
                 stackedBarChart.clear();
                 pieChart.clear();
-                filterApplied = !filterApplied
+                filterApplied = !filterApplied;
+
                 if(filterApplied) {
-                    stackedBarChart = new StackedBarChart(csvData, region.region);
+
+                   
+                    selectedRegion = region;
+                    stackedBarChart = new StackedBarChart(csvData, selectedRegion.region);
                     stackedBarChart.draw();
-                    pieChart = new PieChart(csvData, {region: region.region})
+                    pieChart = new PieChart(csvData, {region: selectedRegion.region})
                     pieChart.draw();
 
                     d3.select('#region-text-container')
                         .append('p')
                         .attr('y', height - 20)
                         .attr('class', 'region-text')
-                        .text(`${region.region} region with ${region.fatalityCount} fatalities over the years`);
+                        .text(`${selectedRegion.region} region with ${selectedRegion.fatalityCount} fatalities over the years`);
+
+                    d3.select(this)
+                        .attr('stroke', 'white')
+                        .attr('stroke-width', 2)
+                        .attr('r', computeRadius(region))
+                        .transition()
+                        .duration(1000)
+                        .attr('r', computeRadius(region) * 1.5);
                 } else {
+
+                    selectedRegion = null;
                     stackedBarChart = new StackedBarChart(csvData);
                     stackedBarChart.draw();
                     pieChart = new PieChart(csvData);
                     pieChart.draw();
-                }
 
-                
+                    d3.selectAll('circle')
+                        .attr('stroke', 'none')
+                        .attr('r', function(d) { return d3.select(this).attr('r'); })
+                        .transition()
+                        .duration(1000)
+                        .attr('r', d => computeRadius(d));
+                }
             })
-            .on('mouseover', region => {
+            .on('mouseover', function(region) {
                 
                 d3.select('#region-text-container').selectAll('.region-text').remove();
                 d3.select('#region-text-container')
-                .append('p')
-                .attr('y', height - 20)
-                .attr('class', 'region-text')
-                .text(`${region.region} region with ${region.fatalityCount} fatalities over the years`);
+                    .append('p')
+                    .attr('y', height - 20)
+                    .attr('class', 'region-text')
+                    .text(`${region.region} region with ${region.fatalityCount} fatalities over the years`);
         
+                d3.select(this)
+                    .attr('stroke', 'white')
+                    .attr('stroke-width', 2);
             })
-            .on('mouseout', region => {
-                
-                // d3.select('#region-text-container').selectAll('.region-text').remove();
+            .on('mouseout', function(region)  {
+                if(!selectedRegion || !selectedRegion == this.region)
+                    d3.select(this)
+                        .attr('stroke', 'none');
             })
             .attr('opacity', 0.8);
     });
 
+    const computeRadius = (region) => {
+        const radius = region.fatalityCount*0.001;
+        if(radius > 5)
+            return radius;
+        else
+            return region.fatalityCount*0.005;
+    }
     return;
 }
